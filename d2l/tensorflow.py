51628cec137323a6bd93ee8df00007e10ee6f402
Mu Li <muli@cs.cmu.edu>
2021-07-28 11:52:34 -0700

    Lib (#1853)
    * partial fix for pandas
    
    * minor touchup on pandas
    
    * update library
    
    Co-authored-by: Aston Zhang <asv325@gmail.com>

--- d2l/tensorflow.py
+++ d2l/tensorflow.py
@@ -1,4 +1,9 @@
-# This file is generated automatically through:
+USE_MXNET = False
+USE_PYTORCH = False 
+USE_TENSORFLOW = True
+
+#################   WARNING   ################
+# The below part is generated automatically through:
 #    d2lbook build lib
 # Don't edit it directly
 
@@ -44,47 +49,36 @@ def set_figsize(figsize=(3.5, 2.5)):
 # Defined in file: ./chapter_preliminaries/calculus.md
 def set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend):
     """Set the axes for matplotlib."""
-    axes.set_xlabel(xlabel)
-    axes.set_ylabel(ylabel)
-    axes.set_xscale(xscale)
-    axes.set_yscale(yscale)
-    axes.set_xlim(xlim)
-    axes.set_ylim(ylim)
+    axes.set_xlabel(xlabel), axes.set_ylabel(ylabel)
+    axes.set_xscale(xscale), axes.set_yscale(yscale)
+    axes.set_xlim(xlim), axes.set_ylim(ylim)
     if legend:
         axes.legend(legend)
     axes.grid()
 
 
 # Defined in file: ./chapter_preliminaries/calculus.md
-def plot(X, Y=None, xlabel=None, ylabel=None, legend=None, xlim=None,
-         ylim=None, xscale='linear', yscale='linear',
-         fmts=('-', 'm--', 'g-.', 'r:'), figsize=(3.5, 2.5), axes=None):
+def plot(X, Y=None, xlabel=None, ylabel=None, legend=[], xlim=None, ylim=None,
+         xscale='linear', yscale='linear', fmts=('-', 'm--', 'g-.', 'r:'),
+         figsize=(3.5, 2.5), axes=None):
     """Plot data points."""
-    if legend is None:
-        legend = []
-
-    set_figsize(figsize)
-    axes = axes if axes else d2l.plt.gca()
-
-    # Return True if `X` (tensor or list) has 1 axis
-    def has_one_axis(X):
+    def has_one_axis(X):  # True if `X` (tensor or list) has 1 axis
         return (hasattr(X, "ndim") and X.ndim == 1 or
                 isinstance(X, list) and not hasattr(X[0], "__len__"))
 
-    if has_one_axis(X):
-        X = [X]
+    if has_one_axis(X): X = [X]
     if Y is None:
         X, Y = [[]] * len(X), X
     elif has_one_axis(Y):
         Y = [Y]
     if len(X) != len(Y):
         X = X * len(Y)
+
+    set_figsize(figsize)
+    if axes is None: axes = d2l.plt.gca()
     axes.cla()
     for x, y, fmt in zip(X, Y, fmts):
-        if len(x):
-            axes.plot(x, y, fmt)
-        else:
-            axes.plot(y, fmt)
+        axes.plot(x, y, fmt) if len(x) else axes.plot(y, fmt)
     set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
 
 
